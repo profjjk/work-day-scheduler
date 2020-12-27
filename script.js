ca309a93ef8e28313dc3d171dc1b2ba5a55db1de
Jordan Kelly <jordan.j.kelly101@gmail.com>
2020-12-27 13:49:12 -0800

    Add text area to html

---
+++ script.js
@@ -0,0 +1,23 @@
+///// CURRENT DAY DISPLAY /////
+
+var date = dayjs().format("dddd, MMMM D")
+$("#currentDay").text(date);
+
+
+///// COLOR-CODED TEXT-AREAS /////
+
+
+///// TYPE TEXT ONTO SCREEN /////
+
+
+///// RETRIEVE TEXT FROM LOCAL STORAGE /////
+
+
+///// SAVE TEXT TO LOCAL STORAGE /////
+// Step One:
+// Q: What are you going to do?
+// A: 
+// Q: How are you going to do it?
+// A:
+// Q: Why are you going to do it?
+// A:
